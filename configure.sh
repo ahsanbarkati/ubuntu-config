0b16790a680a5376ae562cae3be720a0efdae335
Ahsan <ahsanbarkati@gmail.com>
2020-05-02 22:14:01 +0530

    volume

--- configure.sh
+++ configure.sh
@@ -1,9 +1,6 @@
-sudo apt install -y vim
 sudo apt install -y i3 i3-wm i3blocks i3lock i3status
 sudo apt install -y rofi
-sudo apt install -y git
 sudo apt install -y feh
-sudo apt install -y curl
 
 WORKDIR=$(pwd)
 mkdir ~/build
@@ -12,20 +9,22 @@ mkdir ~/build
 sudo apt-get -y install zsh
 cd ~/build
 wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh
-sh install.sh
-sudo apt-get install fonts-powerline
+bash install.sh
+apt-get install fonts-powerline
 fc-cache -vf ~/.fonts/
+sudo rm -R ~/build
 
 # install autojump
+mkdir ~/build
 cd ~/build
 git clone https://github.com/wting/autojump.git
 cd autojump
 ./install.py
-echo "[[ -s /home/$(whoami)/.autojump/etc/profile.d/autojump.sh ]] && source /home/$(whoami)/.autojump/etc/profile.d/autojump.sh
-" >> ~/.bashrc
+echo "[[ -s /home/$(whoami)/.autojump/etc/profile.d/autojump.sh ]] && source /home/$(whoami)/.autojump/etc/profile.d/autojump.sh" >> ~/.bashrc
+
 
 cd $WORKDIR
 # copy the config files
-sudo cp i3_config ~/.config/i3/config
-sudo cp i3blocks.conf /etc/i3blocks.conf
-sudo cp zshrc ~/.zshrc
+sudo cp -v i3_config ~/.config/i3/config
+sudo cp -v i3blocks.conf /etc/i3blocks.conf
+sudo cp -v zshrc ~/.zshrc
